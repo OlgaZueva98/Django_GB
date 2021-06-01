31a3e6654e65bff601e5a4fc6ff257138087727a
OlgaZueva98 <olga.zueva.1998@mail.ru>
2021-06-01 16:55:46 +0300

    Add files via upload

---
+++ geekshop/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import json
+
+def index(request):
+    title = 'geekshop'
+    list_params = ['a1', 'a2', 'a3']
+
+    context = {
+        'list_params': list_params,
+        'some_name': 'hello',
+        'title': title,
+    }
+    return render(request, 'index.html', context=context)
+
+
+def contacts(request):
+    return render(request, 'geekshop/contacts.html')
+
